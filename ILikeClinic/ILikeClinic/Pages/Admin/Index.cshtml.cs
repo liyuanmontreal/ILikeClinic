57b2e9bd4bdde5a8f4b97b653dd320efd5409b1b
Yuan Li <65098066+liyuanmontreal@users.noreply.github.com>
2022-09-20 07:05:28 -0400

    add admin part

---
+++ ILikeClinic/ILikeClinic/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using ILikeClinic.Data;
+using ILikeClinic.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ILikeClinic.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _DB;
+        public IEnumerable<Doctor> Doctors { get; set; } = default!;
+        //public IEnumerable<Doctor> Doctors { get; set; } = default!;
+        public IEnumerable<Message> Messages { get; set; } = default!;
+
+        //Constructor
+        public IndexModel(ApplicationDbContext db)
+        {
+            _DB = db;
+        }
+        public void OnGet()
+        {
+            Doctors = _DB.Doctor;
+            Messages = _DB.Message;
+        }
+    }
+}
+//    @Html.DisplayFor(modelItem => item.dateofBirth)
+//@Html.DisplayFor(modelItem => item.MedicalCardNo)
+
+
