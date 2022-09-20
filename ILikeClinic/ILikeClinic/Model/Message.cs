bfe399319bed854492ca3a4e6b9b9baf57f13c68
Yuan Li <65098066+liyuanmontreal@users.noreply.github.com>
2022-09-20 08:45:26 -0400

    add message table

---
+++ ILikeClinic/ILikeClinic/Model/Message.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ILikeClinic.Model
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        public string FromId { get; set; }
+
+        public string ToId { get; set; }
+
+        public string Text { get; set; }
+
+        public string Status { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:g}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Time")]
+        public DateTime DOB { get; set; }
+
+    }
+
+}
