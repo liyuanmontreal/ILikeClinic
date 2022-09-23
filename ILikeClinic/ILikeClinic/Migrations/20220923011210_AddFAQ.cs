d621293c138704d9720d80686135a52a1747c28e
Xin <zaradudu2195741@users.noreply.github.com>
2022-09-22 21:36:32 -0400

    make appointment page showing

---
+++ ILikeClinic/ILikeClinic/Migrations/20220923011210_AddFAQ.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ILikeClinic.Migrations
+{
+    public partial class AddFAQ : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FAQ",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Question = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Answer = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    FromEmail = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    AspNetUsersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FAQ", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FAQ");
+        }
+    }
+}
